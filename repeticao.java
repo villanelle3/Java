677f67572e925271804bef3562ebbe5fb3a85cef
Laura <106897289+villanelle3@users.noreply.github.com>
2024-01-27 17:02:53 -0300

    Update repeticao.java

--- repeticao.java
+++ repeticao.java
@@ -1,9 +1,10 @@
 public class Main {
   public static void main(String[] args) {
-    int a, b;
-    a = 4;
-    b = 8;
-    System.out.println(a > b);
-  }
 
+    for(int i = 0; i < 10; i++){
+      System.out.println(i + " oi!");
+    }
+      
+
+  }
 }
