2acff8bbd35c94b469d92a208cc6d995f4065406
Laura <106897289+villanelle3@users.noreply.github.com>
2024-01-27 16:04:14 -0300

    Update logicos.java

--- logicos.java
+++ logicos.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     int a, b;
@@ -15,5 +15,32 @@ public class Main {
     }else{
       System.out.println("Reprovado");
     }
+
+    // =======================================
+    Scanner entrada = new Scanner(System.in);
+    System.out.println("Escolha uma opção: ");
+    System.out.println("1 - moto");
+    System.out.println("2 - carro");
+    System.out.println("3 - avião");
+    System.out.println("4 - helicóptero");
+    int numero = entrada.nextInt();
+    switch(numero){
+      case 1:
+        System.out.println("Você escolheu moto");
+        break;
+      case 2:
+        System.out.println("Você escolheu carro");
+        break;
+      case 3:
+        System.out.println("Você escolheu avião");
+        break;
+      case 4:
+        System.out.println("Você escolheu helicóptero");
+        break;
+      default:
+        System.out.println("Opção inválida");
+      
+    }
+    
   }
 }
