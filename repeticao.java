d81e742dc4630aa7a688579fa0fee70db507b9c7
Laura <106897289+villanelle3@users.noreply.github.com>
2024-01-27 17:13:35 -0300

    Update repeticao.java

--- repeticao.java
+++ repeticao.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
 
     for(int i = 0; i < 10; i++){
       System.out.println(i + " oi!");
     }
-      
+
+    // ===============================
+
+    
+    int num = 0;
+    while(num != 99){
+      System.out.println("Digite um número ou 99 para sair: ");
+      Scanner sc = new Scanner(System.in);
+      num = sc.nextInt();
+    }
+    
 
   }
 }
