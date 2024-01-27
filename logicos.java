27f766e756a89d1fa9c5e49215dbfe5545f5d0fb
Laura <106897289+villanelle3@users.noreply.github.com>
2024-01-27 15:51:33 -0300

    Update logicos.java

--- logicos.java
+++ logicos.java
@@ -1,9 +1,19 @@
+
 public class Main {
   public static void main(String[] args) {
     int a, b;
     a = 4;
     b = 8;
     System.out.println(a > b);
+    // =======================================
+    int nota = 4;
+    
+    if(nota >= 5){
+      System.out.println("Aprovado");
+    }else if(nota >= 3 && nota < 5){
+      System.out.println("Exame");
+    }else{
+      System.out.println("Reprovado");
+    }
   }
-
 }
