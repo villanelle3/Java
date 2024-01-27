d5edfb58b02d880cb87f0e5a24449bb688e9ca6b
Laura <106897289+villanelle3@users.noreply.github.com>
2024-01-27 17:39:59 -0300

    Update metodos.java

--- metodos.java
+++ metodos.java
@@ -1,9 +1,12 @@
 public class Main {
-  public static void main(String[] args) {
-    int a, b;
-    a = 4;
-    b = 8;
-    System.out.println(a > b);
+  
+  
+  public static int somar(int a, int b){
+    return a + b;
   }
+  
+  public static void main(String[] args) {
+    System.out.println(somar(2, 3));
 
+  }
 }
