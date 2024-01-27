4776dd5739541bb6b8812d9bbd174c17c5cda63e
Laura <106897289+villanelle3@users.noreply.github.com>
2024-01-27 14:50:24 -0300

    Update variaveis.java

--- variaveis.java
+++ variaveis.java
@@ -1,14 +1,36 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Scanner;
 
-// import org.junit.jupiter.api.Test;
-
-public class Main {
+class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    // System.out.println("Hello world!");
+    String valor = "R$ 1.000,00";
+    String curso = "Curso de Java";
+    String nome;
+    
+    System.out.println(valor);
+    
+    System.out.println(curso.contains("Java"));
+    System.out.println(curso.length());
+    System.out.println(curso.replace("Java", "JavaScript"));
+    System.out.println(curso.toUpperCase());
+    System.out.println(curso.toLowerCase());
+    System.out.println(curso.substring(0, 5));
+    System.out.println(curso.substring(10));
+    // ===========================================================
+    System.out.println("Nome:");
+
+    Scanner entrada = new Scanner(System.in);
+    nome = entrada.nextLine();
+    System.out.println("Olá, " + nome);
   }
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+  // Sytem.out...
+  // println = mostrar na tela, pula linha
+  // print = print, nao pula linha
+  // printf = print formatado, não pula linha
+
+  // System.in... TEM QUE IMPORTAR
+  // entrada de dados
+
+  
 }
