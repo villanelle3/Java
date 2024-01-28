e266530b7e59eff4f850a8b6168e6624ee165488
Laura <106897289+villanelle3@users.noreply.github.com>
2024-01-27 23:33:39 -0300

    vetores, matrizes

---
+++ vetores.java
@@ -0,0 +1,22 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    
+    int[] dados = new int[5];
+    dados[0] = 1;
+    dados[1] = 2;
+    dados[2] = 3;
+    dados[3] = 4;
+    dados[4] = 5;
+    
+    for (int i = 0; i < dados.length; i++){
+      System.out.println(dados[i]);
+    }
+
+
+    
+  }
+}
