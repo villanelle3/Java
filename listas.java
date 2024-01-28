b32929d7098df223760e9301b4c139694ec59d9c
Laura <106897289+villanelle3@users.noreply.github.com>
2024-01-28 01:42:09 -0300

    Create listas.java

---
+++ listas.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+  ArrayLis<String> list = new ArrayList<String>();
+
+    list.add("A");
+
+    
+  }
+}
