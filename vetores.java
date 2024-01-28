3d0faebea69a6114b8073bd19e0226a47c3f3d25
Laura <106897289+villanelle3@users.noreply.github.com>
2024-01-27 23:57:50 -0300

    Update vetores.java

--- vetores.java
+++ vetores.java
@@ -17,6 +17,26 @@ public class Main {
     }
 
 
+    //int[][] matriz = new int[3][3];
+    int total = 0;
+    int totalPrimeiraSemana = 0;
+    int totalSegundaSemana = 0;
+    int[] passarosPorDia = {2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1};
+
+    for (int i = 0; i < passarosPorDia.length; i++){
+      total = total + passarosPorDia[i];
+      if(i < 7){
+        totalPrimeiraSemana = totalPrimeiraSemana + passarosPorDia[i];
+      }else{
+          totalSegundaSemana = totalSegundaSemana + passarosPorDia[i];
+      }
+    }
+
+
+
+    System.out.println("Total de passaros: " + total);
+    System.out.println("Total de passaros na primeira semana: " + totalPrimeiraSemana);
+    System.out.println("Total de passaros na segunda semana: " + totalSegundaSemana);
     
-  }
+  };
 }
