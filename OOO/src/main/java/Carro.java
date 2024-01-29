a2f01370418ff34ac9a240392771ae81021418d9
Laura <106897289+villanelle3@users.noreply.github.com>
2024-01-29 01:55:12 -0300

    new

---
+++ OOO/src/main/java/Carro.java
@@ -0,0 +1,13 @@
+Class Carro{
+  String nome;
+  String cor;
+  int ano;
+  double peso;
+
+  public void Anda(){
+    System.out.println("O carro está andando");
+  }
+  public void Freia(){
+    System.out.println("O carro está freiando");
+  }
+}
