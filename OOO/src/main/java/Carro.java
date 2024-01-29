e9c577e2e496e907b7abd5a8ff4179872b1ffccf
Laura <106897289+villanelle3@users.noreply.github.com>
2024-01-29 09:04:59 -0300

    Update Carro.java

--- OOO/src/main/java/Carro.java
+++ OOO/src/main/java/Carro.java
@@ -4,10 +4,28 @@ Class Carro{
   int ano;
   double peso;
 
+  public Carro(){
+    // todos os carros começam com 500 kg
+    peso = 500.0;
+  }
+
+  public Carro(String nome, String cor, int ano, double peso){
+    this.nome = nome;
+    this.cor = cor;
+    this.ano = ano;
+    this.peso = peso;
+  }
+
   public void Anda(){
     System.out.println("O carro está andando");
   }
   public void Freia(){
     System.out.println("O carro está freiando");
+  }
+  public void Dados(){
+    System.out.println("Nome: " + nome);
+    System.out.println("Cor: " + cor);
+    System.out.println("Ano: " + ano);
+    System.out.println("Peso: " + peso);
   }
 }
