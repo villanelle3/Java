0f9f34911c4371628989e9aa257fdb930158f189
Laura <106897289+villanelle3@users.noreply.github.com>
2024-01-29 09:32:47 -0300

    new files

---
+++ poo2/src/main/java/Cao.java
@@ -0,0 +1,23 @@
+public Class Cao{
+  private int idade; // Só tem acesso dentro da classe
+
+  public void setIdade(int idade){
+    idade = idade;
+  }
+  public int getIdade(){
+    return idade;
+  }
+  
+  public void Andar(){
+    System.out.println("Andando");
+  }
+
+  public boolean VerificarIdade(){
+    if(idade > 10){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  
+}
---
+++ poo2/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    Cao cachorro = new Cao();
+    cachorro.setIdade(5);
+    if(cahorro.VerificarIdade() == true){
+      System.out.println("Idoso");
+    }else{
+      System.out.println("Novinho");
+    }
+  }
+}
